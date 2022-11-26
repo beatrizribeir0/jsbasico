c2b8d299dfc74a4069ed91c03852d6f74e2baf72
beatrizribeir0 <biasilvabr58@gmail.com>
2022-11-26 15:31:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt("Digite o primeiro numero"));
+        let num2 = parseInt(prompt("Digite o segundo numero"));
+        
+        let resultado = num1 + num2;
+
+        alert('${num1} - ${num2} = ${resultado}');
